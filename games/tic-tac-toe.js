20e8f118547de245b5bd4d823e9677b5da896adb
Riyan-20217 <y23787041@gmail.com>
2025-03-17 12:23:48 +0530

    Update tic-tac-toe.js

--- games/tic-tac-toe.js
+++ games/tic-tac-toe.js
@@ -1,1 +1,44 @@
+let board = ["", "", "", "", "", "", "", "", ""];
+let currentPlayer = "X";
+let gameActive = true;
+
+function makeMove(index) {
+    if (board[index] === "" && gameActive) {
+        board[index] = currentPlayer;
+        document.getElementsByClassName("cell")[index].innerText = currentPlayer;
+        checkWinner();
+        currentPlayer = currentPlayer === "X" ? "O" : "X";
+        document.getElementById("status").innerText = `Player ${currentPlayer}'s Turn`;
+    }
+}
+
+function checkWinner() {
+    const winningCombos = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
+    ];
+    
+    for (let combo of winningCombos) {
+        let [a, b, c] = combo;
+        if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+            document.getElementById("status").innerText = `Player ${board[a]} Wins!`;
+            gameActive = false;
+            return;
+        }
+    }
+    
+    if (!board.includes("")) {
+        document.getElementById("status").innerText = "It's a Draw!";
+        gameActive = false;
+    }
+}
+
+function resetGame() {
+    board = ["", "", "", "", "", "", "", "", ""];
+    gameActive = true;
+    currentPlayer = "X";
+    document.getElementById("status").innerText = "Player X's Turn";
+    document.querySelectorAll(".cell").forEach(cell => cell.innerText = "");
+}
 
